42ae29c98ee9b52903907451f47650da1274d507
Rafaelysilv <fernanda.rafaelysilva@gmail.com>
2026-09-01 17:50:04 -0300

    comentário

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+
+app.use(express.json())
+app.use(cors({
+    origin: ['http://localhost:3333', 'http://127.0.0.1:3333']
+}))
+
+const alunos = [
+    {"nome": "", "url": ""}
+]
+
+app.get('/alunos', async (req, res) => {
+    try {
+        const [rows] = await db.query("SELECT id, nome ,senha FROM alunos")
+        res.json(rows)
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: "erro ao buscar alunos" })
+
+    }
+})
+
+app.post('/alunos', (req, res) => {
+    const { nome, url } = req.body
+    res.json({"nome": nome, "url": url})
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3333')
+})
